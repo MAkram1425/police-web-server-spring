70627d5913b5b0d17bc4094c70fb0eac11cf6879
fahad-qureshi786 <mfahad.bses19@iba-suk.edu.pk>
2022-07-13 11:18:54 +0500

    create baseline for police web

---
+++ src/main/java/com/smart/policeweb/PoliceWebApplication.java
@@ -0,0 +1,13 @@
+package com.smart.policeweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliceWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoliceWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smart/policeweb/PoliceWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart.policeweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoliceWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
